9a21e03c9bba5adb63525bfbe8595499461351ca
xmeng <mxf3306@gmail.com>
2018-11-08 21:39:11 +0000

    Add unit test

---
+++ src/test/java/org/xmeng/xmeng/DataTest.java
@@ -0,0 +1,38 @@
+package org.xmeng.xmeng;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DataTest {
+
+    @Test
+    public void testInValidData() {
+        Data data = new Data();
+        data.longitude = 0;
+        data.latitude = 0;
+
+        Assert.assertFalse(data.valid());
+
+        data.longitude = 53;
+
+        Assert.assertFalse(data.valid());
+
+        data.longitude = 51.8;
+        Assert.assertFalse(data.valid());
+    }
+
+    @Test
+    public void testValidData() {
+        Data data = new Data();
+        data.longitude = 4;
+        data.latitude = 51.8;
+
+        Assert.assertTrue(data.valid());
+
+        data.longitude = 3.9;
+        data.latitude = 51.9;
+
+        Assert.assertTrue(data.valid());
+    }
+}
